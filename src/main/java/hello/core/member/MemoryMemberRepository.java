295d9429cfc185946b5def68014461be418f1a09
changeun <ypd05172@naver.com>
2022-10-12 23:09:51 +0900

    회원 도메인 비즈니스 요구사항 설계 및 개발

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,21 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+//    ConcurrentHashMap 을 사용하면 좋다. 그냥 HashMap은 동시성 이슈가 있을 수 있기 때문에 동시성 문제를 해결할 수 있는 ConcurrentHashMap 을 사용할 수 있다.(참고)
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
