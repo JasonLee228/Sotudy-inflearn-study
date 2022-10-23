823f752e6f2f43caf8b20c62ca79fcd3d96742a3
changeun <ypd05172@naver.com>
2022-10-23 17:18:51 +0900

    AppConfig 를 통해 OCP, DIP 를 준수하며 할인 정책을 변경함

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -13,6 +14,7 @@ import hello.core.order.OrderServiceImpl;
  * TODO : 애플리케이션의 전체 동작 방식을 구성(config)하기 위해, 구현 객체를 생성하고, 연결하는 책임을 가지는 별도의 설정 클래스를 만들자
  *        구현체에서 직접 선택하여 주입하는 것이 아닌, 별도 클래스에서 필요 객체를 주입해 주자
  *        애플리케이션의 실제 동작에 필요한 구현 객체를 생성자를 통해 주입한다
+ *        AppConfig : 애플리케이션을 사용 영역과, 구성(Configuration)하는 영역으로 분리됨
  * */
 public class AppConfig {
 
@@ -31,8 +33,9 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
-
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy(); // 정액 할인 정책에서 정률 할인 정책으로 변경 -> 단 한줄만 변경하면 돼!
+                                         // 사용 영역을 변경하지 않고, 구성 영역의 변경만으로 정책 변경이 가능
     }
 
     /**
