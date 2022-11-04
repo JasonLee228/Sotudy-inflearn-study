5278e5e5ffa62b08bbec86ea234f8563c87f1257
changeun.lee <loveck228@gmail.com>
2022-11-05 00:25:34 +0900

    xml 을 통한 빈 설정 예제

---
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,20 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        // resources 내의 appConfig.xml 파일을 가져와 빈 설정 정보로 사용한다.
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+
+    }
+}
