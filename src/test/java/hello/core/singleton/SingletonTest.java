17843f58cdd1786f3e2c8c5388106ed0df55ba2c
changeun.lee <loveck228@gmail.com>
2022-11-06 00:08:04 +0900

    스프링 없는 순수 DI 컨테이너를 통한 싱글톤 패턴의 필요성 확인

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -1,2 +1,32 @@
-package hello.core.singleton;public class SingletonTest {
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+
+        // 스프링 없는 순수 DI 컨테이너는, 요청이 들어올 때마다 객체를 매번 새로 생성
+        // -> 메모리 낭비가 심하다
+        AppConfig appConfig = new AppConfig();
+
+        // 1. 조회 : 호출할 때 마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+
+        // 2. 조회 : 호출할 때 마다 객체를 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        // 1, 2 의 참조값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberService1 != memberService2
+        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+    }
 }
