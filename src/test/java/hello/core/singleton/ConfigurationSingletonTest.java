4928513472fad52efb1d257977310a4bd20fb117
changeun.lee <loveck228@gmail.com>
2022-11-09 00:01:32 +0900

    @Configuration 이 붙은 클래스는 싱글톤을 보장하기 위해 스프링에서 새로 클래스를 만들어 빈으로 등록함을 알 수 있다.

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -34,4 +34,15 @@ public class ConfigurationSingletonTest {
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
 
     }
+    
+    @Test
+    void configurationDeep() {
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean.getClass() = " + bean.getClass());
+        // bean.getClass() = class hello.core.AppConfig$$EnhancerBySpringCGLIB$$5881c3ba
+    }
 }
