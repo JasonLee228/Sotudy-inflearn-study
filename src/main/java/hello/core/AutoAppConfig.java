75403687d879e1576e85cd5d332eac5fcc4ccbae
changeun.lee <loveck228@gmail.com>
2022-11-16 00:57:56 +0900

    컴포넌트 스캔의 탐색 위치 지정 방법

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,7 +7,11 @@ import org.springframework.context.annotation.FilterType;
 * TODO: 컴포넌트 스캔을 통한 빈 등록
 * */
 @Configuration
-@ComponentScan( // 스캔 대상에서 제외할 항목 선택, @Configuration 은 자동으로 빈으로 올라가선 안되기 때문에(기존 코드 때문) 제외 설정
+@ComponentScan(
+        // 스캔을 시작할 위치 지정. 해당 패키지부터 하위 패키지를 확인하여 빈을 등록한다.
+        // 무분별한 컴포넌트 스캔을 방지
+        basePackages = "hello.core",// 여러개 지정도 가능
+        // 스캔 대상에서 제외할 항목 선택, @Configuration 은 자동으로 빈으로 올라가선 안되기 때문에(기존 코드 때문) 제외 설정
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
