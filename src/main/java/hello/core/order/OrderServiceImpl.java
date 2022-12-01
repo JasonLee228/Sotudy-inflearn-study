eadfdd51a687102e2f2ca6f11e34ec9e9cad5ffa
changeun <ypd05172@naver.com>
2022-12-01 21:02:16 +0900

    OrderServiceImpl 주석 제거

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -12,21 +12,6 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
-/*
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
-     할인 정책의 변경 : 정액 -> 정률
-    * DIP: 주문서비스 클라이언트( OrderServiceImpl )는 DiscountPolicy 인터페이스에 의존하면서 DIP 를 지킨 것 같은데?
-    클래스 의존관계를 분석해 보자. 추상(인터페이스) 뿐만 아니라 구체(구현) 클래스에도 의존하고 있다.
-        추상(인터페이스) 의존: DiscountPolicy
-        구체(구현) 클래스: FixDiscountPolicy , RateDiscountPolicy
-    * OCP: 변경하지 않고 확장할 수 있다고 했는데!
-        지금 코드는 기능을 확장해서 변경하면, 클라이언트 코드에 영향을 준다! 따라서 OCP 를 위반한다.
-    해결 : DIP, OCP 를 위반하지 않도록 , 인터페이스에만 의존하도록 의존관계를 변경하면 된다
-    -> 구체 선언 부분 자체를 보이지 않게 하면 된다! : 누군가 클라이언트에 구현 객체를 대신 생성 후 주입해주어야 한다.
-*/
-    // final 키워드가 붙으면 초기 값 세팅 또는 생성자를 통해서만 값을 세팅할 수 있다.
-    // final 키워드가 있기 때문에 값 세팅 여부 등을 확인할 수 있다.
     private final MemberRepository memberRepository;
     @MainDiscountPolicy
     private final DiscountPolicy discountPolicy;
@@ -52,6 +37,7 @@ public class OrderServiceImpl implements OrderService{
 */
 
 /*
+    // 메서드 주입
     private MemberRepository memberRepository;
     private DiscountPolicy discountPolicy;
 
