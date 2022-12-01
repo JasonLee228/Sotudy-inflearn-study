afd04561606e41c7c5faf98aa04c5a867eaf6dd2
changeun <ypd05172@naver.com>
2022-12-01 21:48:48 +0900

    특정 클래스에서 특정 타입의 빈을 모두 주입받아 사용해야 할 때, 조회한 빈이 모두 필요할 때 -> 빈 목록 Map 사용

---
+++ src/test/java/hello/core/autowired/AllBeanTest.java
@@ -0,0 +1,57 @@
+package hello.core.autowired;
+
+import hello.core.AutoAppConfig;
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class AllBeanTest {
+
+    @Test
+    void findAllBean() {
+
+        // 컴포넌트 스캔으로 빈을 등록하는 대상인 AutoAppConfig 클래스를 함께 등록해 discountPolicy 들을 가져올 수 있게 한다.
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "userA", Grade.VIP);
+
+        int fixDiscountPrice = discountService.discount(member, 10000, "fixDiscountPolicy");
+        int rateDiscountPrice = discountService.discount(member, 20000, "rateDiscountPolicy");
+
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(fixDiscountPrice).isEqualTo(1000);
+        assertThat(rateDiscountPrice).isEqualTo(2000);
+
+    }
+
+    static class DiscountService {
+
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policies;
+
+        // AutoAppConfig 를 통해 모든 DiscountPolicy 를 주입받게 된다.
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+            this.policyMap = policyMap;
+            this.policies = policies;
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policies = " + policies);
+        }
+
+        public int discount(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            return discountPolicy.discount(member, price);
+        }
+
+    }
+
+}
