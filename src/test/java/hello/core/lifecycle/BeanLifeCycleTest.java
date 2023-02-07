e2ebc0b67f8b4ccff9f7fbf5c393b0196e875100
changeun.lee <loveck228@gmail.com>
2023-02-08 00:04:30 +0900

    230208 빈 생성주기 초기 로직 생성, 초기화 메소드를 통한 최초 설정이 제대로 이루어지지 않음을 볼 수 있음.

---
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,32 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+
+        // 기존의 ApplicationContext 와는 다르다는걸 인지.
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("https://sotudy.com");
+            return networkClient;
+        }
+    }
+
+}
---
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,36 @@
+package hello.core.lifecycle;
+
+public class NetworkClient {
+    /**
+     * 간단하게 외부 네트워크에 미리 연결하는 객체를 하나 생성한다고 가정해보자.
+     * 실제로 네트워크에 연결하는 것은 아니고, 단순히 문자만 출력하도록 했다.
+     * 이 NetworkClient 는 애플리케이션 시작 시점에 connect() 를 호출해서 연결을 맺어두어야 하고,
+     * 애플리케이션이 종료되면 disconnect() 를 호출해 연결을 끊어야 한다
+     */
+
+    private String url;
+
+    public NetworkClient() {
+
+        System.out.println("생성자 호출, url = " + url);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작시 호출
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call : " + url + ", message : " + message);
+    }
+
+    // 서비스 종료시 호출
+    public void disconnect() {
+        System.out.println("close : " + url);
+    }
+
+}
