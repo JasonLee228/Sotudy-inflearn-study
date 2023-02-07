da164aec0abf3842bc03f9d17e8dd8f810543b32
changeun.lee <loveck228@gmail.com>
2023-02-08 00:29:34 +0900

    230208 스프링 인터페이스인 InitializingBean, DisposableBean 을 통해 빈 생명주기 콜백 지정

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,6 +1,9 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
     /**
      * 간단하게 외부 네트워크에 미리 연결하는 객체를 하나 생성한다고 가정해보자.
      * 실제로 네트워크에 연결하는 것은 아니고, 단순히 문자만 출력하도록 했다.
@@ -13,8 +16,7 @@ public class NetworkClient {
     public NetworkClient() {
 
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
+
     }
 
     public void setUrl(String url) {
@@ -35,4 +37,18 @@ public class NetworkClient {
         System.out.println("close : " + url);
     }
 
+    // 의존관계 주입이 끝나면 호출
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    // 빈 종료시 호출
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
 }
