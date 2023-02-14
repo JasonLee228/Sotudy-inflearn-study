844920b1f5ace196fa1e1ae02b87fc651a8ce16a
changeun.lee <loveck228@gmail.com>
2023-02-15 00:06:55 +0900

    230214 request 스코프 예제 만들기(2). provider 를 이용해 의존성 주입 시기를 조절할 수 있다

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,19 +14,19 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger; // 중요! 실행 안되는 이유 아래.
+    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     /**
-     * 현재 코드 상태에서는 스프링은 뜰 수 없다.
-     * MyLogger 클래스의 스코프는 request 이기 때문에
-     * 요청이 들어오지 않으면 active 되지 않는데, 의존성 주입을 스프링 생성 시점에서 주입시키려고 하니 오류가 발생한다.
-     * 요청이 들어오지 않아 생성되지 않은 없는 빈을 주입하려 해서.
-     * -> 주입 시기를 요청이 왔을 때로 미뤄야 한다.!
+     * 스프링이 실행되지 않던 문제를 provider 를 통해 실제 요청이 들어온 시점에
+     * provider.getObject() 를 통해 주입받을 수 있도록 했다.
+     * 때문에 의존성이 필요할 때 주입될 수 있고, 오류 발생하지 않고 스프링이 실행될 수 있다.
      */
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
+
+        MyLogger myLogger = myLoggerProvider.getObject(); // 이 시점에 해당 request 에 대한 빈 생성 및 의존성 주입
 
         String requestURL = request.getRequestURL().toString();
 
